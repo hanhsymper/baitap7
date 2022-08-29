18b3e286453ed60464a825e0627379ffa522fa3a
hanhsymper <hanhdth@symper.vn>
2022-08-29 11:17:41 +0700

    data test product

--- tests/test_boose_convert.spec.ts
+++ tests/test_boose_convert.spec.ts
@@ -8,7 +8,7 @@ test('test when click button save in screen real time', async ({dashBoard, page 
     // await RealTimeVisitors.onOffRealTime('bat')
     await RealTimeVisitors.clickButtoChange();
     await RealTimeVisitors.fillDataIntoInput(REALTIME.random_from,REALTIME.random_to);
-    await RealTimeVisitors.clickTrigger(REALTIME.trigger,REALTIME.type_display,REALTIME.arr_product)
+    let count=await RealTimeVisitors.clickTrigger(REALTIME.trigger,REALTIME.type_display,REALTIME.arr_product)
     await RealTimeVisitors.clickButtonSave();
 
     await expect(page.locator(`//form[@class='s-form']//span[contains(text(),"${REALTIME.trigger}")]`)).toBeChecked();
@@ -20,18 +20,18 @@ test('test when click button save in screen real time', async ({dashBoard, page 
     await expect(random_to).toEqual(z);
     // let count_pr=await RealTimeVisitors.getQuantityDisplay(".mdi.mdi-minus-circle-outline.mdi-24px");
 
-    // let count_pr_select=Number(count_pr)+Number(REALTIME.arr_product.length);
-    // console.log(Number(count_pr))
+    let count_pr_select=Number(count)+Number(REALTIME.arr_product.length);
+    console.log(Number(count))
     
-    // if(REALTIME.type_display==='product'){
-    //     await expect(page.locator("//div[@class='s-ml24']/span")).toContainText(count_pr_select+" products selected")
-    //    // 2 products selected
-    // }else{
-    //     // 1 collection selected
-    //     await expect(page.locator("//div[@class='s-ml24']/span")).toContainText(count_pr_select+" collection selected")
+    if(REALTIME.type_display==='product'){
+        await expect(page.locator("//div[@class='s-ml24']/span")).toContainText(count_pr_select+" products selected")
+       // 2 products selected
+    }else{
+        // 1 collection selected
+        await expect(page.locator("//div[@class='s-ml24']/span")).toContainText(count_pr_select+" collection selected")
 
-    //     // await expect(page.locator("//div[@class='s-ml24']/span"))
-    // }
+        // await expect(page.locator("//div[@class='s-ml24']/span"))
+    }
 
 
 });
